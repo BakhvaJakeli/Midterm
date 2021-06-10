cff5185617d034bea422d973222c306563705f55
BakhvaJakeli <lordbaxva@gmail.com>
2021-06-10 19:56:56 +0400

    added Models

--- MidTerm/Models.swift
+++ MidTerm/Models.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+struct Welcome: Codable {
+    let games: [Game]
+}
+
+// MARK: - Game
+struct Game: Codable {
+    let questions: [Question]
+}
+
+// MARK: - Question
+struct Question: Codable {
+    let question: String
+    let content: [String]
+    let correct: Int
+}
